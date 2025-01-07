5e13c7ba36dbb43993ba224342bd15c85dad02d1
JaeMin <dnfktmzm@naver.com>
2025-01-07 15:11:44 +0900

    당근마켓 홈페이지 프로필 이미지로 사용중인 외부 URL에 대한 접근 허용 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "avatars.githubusercontent.com"
+            },
+            {
+                hostname: "i.namu.wiki"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
